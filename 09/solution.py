3c1cb8f48c7fba8143ff18826f1c2b0c74f56789
Enayatullah Norozi <enayathushmand12@gmail.com>
2022-05-08 02:30:24 +0200

    run formatter

--- 09/solution.py
+++ 09/solution.py
@@ -2,8 +2,10 @@ preamble_len = 25
 
 
 def find_sum(sum, preamble):
-    list_of_lists = [[int(x)+int(y) for x in preamble if x !=
-                      y if int(x)+int(y) == int(sum)] for y in preamble]
+    list_of_lists = [
+        [int(x) + int(y) for x in preamble if x != y if int(x) + int(y) == int(sum)]
+        for y in preamble
+    ]
     flattened = [val for sublist in list_of_lists for val in sublist]
     return int(sum) in flattened
 
@@ -12,7 +14,7 @@ def solve_one(inputs):
     preamble = inputs[:preamble_len]
     sums = inputs[preamble_len:]
     for i in range(len(sums)):
-        index = i+preamble_len+1
+        index = i + preamble_len + 1
         if not find_sum(sums[i], preamble):
             return int(sums[i])
         preamble = inputs[:index][-preamble_len:]
@@ -23,15 +25,15 @@ def solve_two(inputs):
     inv = solve_one(inputs)
     for i in range(len(inputs)):
         nums = []
-        sum = 0
+        my_sum = 0
         j = 0
-        while sum < inv:
-            sum += int(inputs[i+j])
-            nums.append(int(inputs[i+j]))
+        while my_sum < inv:
+            my_sum += int(inputs[i + j])
+            nums.append(int(inputs[i + j]))
             j += 1
-        if sum == inv:
+        if my_sum == inv:
             nums.sort()
-            return nums[0]+nums[-1]
+            return nums[0] + nums[-1]
     return -1
 
 
