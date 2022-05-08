273b96fc2661fd71e7708d7bbd6e5a5786c3621d
Enayatullah Norozi <enayathushmand12@gmail.com>
2022-05-08 02:30:37 +0200

    use caching to find all combos for part 2

--- 10/solution.py
+++ 10/solution.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 def possibles(output, ratings):
     return [x for x in ratings if x <= output+3 and x > output]
 
@@ -12,18 +14,26 @@ def solve_one(ratings):
             output = possible[0]
     return diffs.count(1)*(diffs.count(3)+1)
 
-
 def solve_two(ratings):
-    return "Ofcoarse I don't know dynamic programming"
+    goal = max(ratings)
+
+    @cache
+    def combs(output: int) -> int:
+        if output == goal:
+            return 1
+        possible = possibles(output, ratings)
+        if len(possible) == 0:
+            return 0
+        return sum(combs(poss) for poss in possible)
+
+    return combs(0)
 
 
 def main():
-    f = open("input1.txt", "r")
-    input = list(map(lambda x: int(x), f.read().split()))
-    input.sort()
-
-    print(solve_one(input))
-    print(solve_two(input))
+    f = open("input.txt", "r")
+    inp = sorted(map(lambda x: int(x), f.read().split()))
+    print(solve_one(inp))
+    print(solve_two(inp))
 
 
 main()
